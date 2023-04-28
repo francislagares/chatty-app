51d92c537f819d6212fb66ee96c599ab8ea69660
Francis Lagares <francis.lagares@gmail.com>
2023-04-28 11:19:50 +0200

    feat(post): create joi validation schema

---
+++ server/src/features/post/schemas/post.ts
@@ -0,0 +1,54 @@
+import Joi, { ObjectSchema } from 'joi';
+
+const postSchema: ObjectSchema = Joi.object().keys({
+  post: Joi.string().optional().allow(null, ''),
+  bgColor: Joi.string().optional().allow(null, ''),
+  privacy: Joi.string().optional().allow(null, ''),
+  feelings: Joi.string().optional().allow(null, ''),
+  gifUrl: Joi.string().optional().allow(null, ''),
+  profilePicture: Joi.string().optional().allow(null, ''),
+  imgVersion: Joi.string().optional().allow(null, ''),
+  imgId: Joi.string().optional().allow(null, ''),
+  image: Joi.string().optional().allow(null, ''),
+  video: Joi.string().optional().allow(null, ''),
+  videoVersion: Joi.string().optional().allow(null, ''),
+  videoId: Joi.string().optional().allow(null, ''),
+});
+
+const postWithImageSchema: ObjectSchema = Joi.object().keys({
+  image: Joi.string().required().messages({
+    'any.required': 'Image is a required field',
+    'string.empty': 'Image property is not allowed to be empty',
+  }),
+  post: Joi.string().optional().allow(null, ''),
+  video: Joi.string().optional().allow(null, ''),
+  bgColor: Joi.string().optional().allow(null, ''),
+  privacy: Joi.string().optional().allow(null, ''),
+  feelings: Joi.string().optional().allow(null, ''),
+  gifUrl: Joi.string().optional().allow(null, ''),
+  profilePicture: Joi.string().optional().allow(null, ''),
+  imgVersion: Joi.string().optional().allow(null, ''),
+  imgId: Joi.string().optional().allow(null, ''),
+  videoVersion: Joi.string().optional().allow(null, ''),
+  videoId: Joi.string().optional().allow(null, ''),
+});
+
+const postWithVideoSchema: ObjectSchema = Joi.object().keys({
+  video: Joi.string().required().messages({
+    'any.required': 'Video is required',
+    'string.empty': 'Video property is not allowed to be empty',
+  }),
+  image: Joi.string().optional().allow(null, ''),
+  post: Joi.string().optional().allow(null, ''),
+  bgColor: Joi.string().optional().allow(null, ''),
+  privacy: Joi.string().optional().allow(null, ''),
+  feelings: Joi.string().optional().allow(null, ''),
+  gifUrl: Joi.string().optional().allow(null, ''),
+  profilePicture: Joi.string().optional().allow(null, ''),
+  imgVersion: Joi.string().optional().allow(null, ''),
+  imgId: Joi.string().optional().allow(null, ''),
+  videoVersion: Joi.string().optional().allow(null, ''),
+  videoId: Joi.string().optional().allow(null, ''),
+});
+
+export { postSchema, postWithImageSchema, postWithVideoSchema };
